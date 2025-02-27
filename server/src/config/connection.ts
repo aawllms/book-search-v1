68f0f4dbea36aa1c6fa4e0d4861278ce81690ac7
aawllms <aarondwilliams8@gmail.com>
2025-02-27 10:06:49 -0700

    update server and connection file to have db in it to allow login and create user

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
+);
 
-export default mongoose.connection;
+const db = mongoose.connect;
+
+export default db;
